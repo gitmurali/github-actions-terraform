bf33ffbafd0fe0748d6dd108f9e41cd6d03bd6d3
gitmurali <murali.nutalapatii@gmail.com>
2024-06-01 12:24:35 +0530

    update kms key

--- main.tf
+++ main.tf
@@ -4,11 +4,6 @@ provider "aws" {
 
 resource "aws_s3_bucket" "example" {
   bucket = "my-tf-example-bucket-999"
-   acl    = "private"
-
-   versioning {
-        enabled = true
-    }
 }
 
 resource "aws_s3_bucket_ownership_controls" "example" {
@@ -32,6 +27,29 @@ resource "aws_s3_bucket_public_access_block" "example" {
   block_public_policy     = true
   ignore_public_acls      = true
   restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_versioning" "versioning_example" {
+  bucket = aws_s3_bucket.example.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_kms_key" "mykey" {
+  description             = "This key is used to encrypt bucket objects"
+  deletion_window_in_days = 7
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "example" {
+  bucket = aws_s3_bucket.example.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      kms_master_key_id = aws_kms_key.mykey.arn
+      sse_algorithm     = "aws:kms"
+    }
+  }
 }
 
 terraform {
