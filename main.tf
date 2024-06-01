4d8dc50a62ee3cf1260693903ebea146d14e01d9
gitmurali <murali.nutalapatii@gmail.com>
2024-06-01 12:15:55 +0530

    update main

--- main.tf
+++ main.tf
@@ -5,6 +5,10 @@ provider "aws" {
 resource "aws_s3_bucket" "example" {
   bucket = "my-tf-example-bucket-999"
    acl    = "private"
+
+   versioning {
+        enabled = true
+    }
 }
 
 resource "aws_s3_bucket_ownership_controls" "example" {
@@ -19,6 +23,15 @@ resource "aws_s3_bucket_acl" "example" {
 
   bucket = aws_s3_bucket.example.id
   acl    = "private"
+}
+
+resource "aws_s3_bucket_public_access_block" "example" {
+  bucket = aws_s3_bucket.example.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
 
 terraform {
