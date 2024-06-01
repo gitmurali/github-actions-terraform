4edcbb18d3213b106d30c20603d2d0f22efa5f2b
gitmurali <murali.nutalapatii@gmail.com>
2024-06-01 12:09:24 +0530

    update main

--- main.tf
+++ main.tf
@@ -2,8 +2,22 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_s3_bucket" "example_bucket" {
-  bucket = "my-example-bucket-name-222"
+resource "aws_s3_bucket" "example" {
+  bucket = "my-tf-example-bucket-999"
+}
+
+resource "aws_s3_bucket_ownership_controls" "example" {
+  bucket = aws_s3_bucket.example.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_acl" "example" {
+  depends_on = [aws_s3_bucket_ownership_controls.example]
+
+  bucket = aws_s3_bucket.example.id
+  acl    = "private"
 }
 
 terraform {
